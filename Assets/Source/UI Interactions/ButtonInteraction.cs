2dab039eb352d2aac8b3ae019ed976ab04e9c413
Yasmine Weiwert <yasmine.weiwert@gmail.com>
2020-02-01 22:29:07 +0100

    ButtonInteractionScript

---
+++ Assets/Source/UI Interactions/ButtonInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInteraction : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject SeasonScreen;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //PressStart();
+    }
+
+    public void PressStart()
+    {
+        MainMenu.SetActive(false);
+        SeasonScreen.SetActive(true);
+    }
+}
