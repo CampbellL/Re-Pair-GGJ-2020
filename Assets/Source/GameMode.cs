8f70e2d9b35b6703fe80973fdc39e15207b8ee8c
paulbichler <Paulbichler@hotmail.de>
2020-02-02 07:46:23 +0100

    sds
    sdds

---
+++ Assets/Source/GameMode.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameMode : MonoBehaviour
+{
+    public int tries;
+    private Animal[] animals;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animals = FindObjectsOfType<Animal>().Where();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Play()
+    {
+        
+    }
+}
