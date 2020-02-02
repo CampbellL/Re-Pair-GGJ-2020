270e4fb637f32cce9b41dd6e9f0269d5501d4d83
Yasmine Weiwert <yasmine.weiwert@gmail.com>
2020-02-02 10:06:03 +0100

    Buttons got Sound

---
+++ Assets/Source/UI Interactions/ButtonSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        Button b = GetComponent<Button>();
+        AudioSource audio = Camera.main.GetComponent<AudioSource>();        b.onClick.AddListener(delegate () {
+            audio.Play();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
